c92b36b1d0f7b656740cbd39214239b2bb7344be
Troy Dai <git@troydai.com>
2021-08-29 21:00:21 -0700

    tserver: add client request close

--- tserver/main.go
+++ tserver/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -9,11 +10,26 @@ import (
 )
 
 type handler struct {
+	closec chan struct{}
 }
 
 type payload struct {
 	Summary    string
 	StatusCode int
+}
+
+func newHandler() *handler {
+	return &handler{
+		closec: make(chan struct{}),
+	}
+}
+
+func (h *handler) CloseChannel() <-chan struct{} {
+	return h.closec
+}
+
+func (h *handler) close() {
+	h.closec <- struct{}{}
 }
 
 func (h *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
@@ -22,6 +38,11 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	switch req.Method {
 	case "GET":
 		p.Summary = fmt.Sprintf("GET %s", req.RequestURI)
+	case "POST":
+		p.Summary = fmt.Sprintf("POST %s", req.RequestURI)
+		if req.RequestURI == "/close" {
+			h.close()
+		}
 	default:
 		p.Summary = fmt.Sprintf("unknown method %s", req.Method)
 		p.StatusCode = http.StatusBadRequest
@@ -48,12 +69,23 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 func main() {
 	fmt.Println("Starting test HTTP server ...")
 
+	h := newHandler()
+
 	s := &http.Server{
 		Addr:         ":8080",
-		Handler:      &handler{},
+		Handler:      h,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
 
-	s.ListenAndServe()
+	go func() {
+		s.ListenAndServe()
+	}()
+
+	<-h.CloseChannel()
+	fmt.Println("Exiting the server ...")
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+
+	s.Shutdown(ctx)
 }
