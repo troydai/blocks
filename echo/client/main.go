ce81026fb96608a5ded881bba9e79cfeda0e23bb
Troy Dai <git@troydai.com>
2021-09-23 19:52:32 -0700

    echo: demostrate metadata

--- echo/client/main.go
+++ echo/client/main.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/troydai/blocks/echo/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 func main() {
@@ -22,10 +24,22 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	resp, err := client.Echo(ctx, &proto.EchoRequest{Message: "well, i'm waiting."})
+	m := metadata.New(map[string]string{"reqctx-persist": "from-edge"})
+	ctx = metadata.NewOutgoingContext(ctx, m)
+
+	req := &proto.EchoRequest{Message: extractMessage()}
+
+	resp, err := client.Echo(ctx, req)
 	if err != nil {
 		log.Fatalf("fail to communicate with the server")
 	}
 	fmt.Printf("message: %s\n", resp.Message)
-	fmt.Printf(" digest: %s\n", resp.Digest)
+	fmt.Printf(" digest: %x\n", resp.Digest)
+}
+
+func extractMessage() string {
+	if len(os.Args) < 2 {
+		return "test message"
+	}
+	return os.Args[1]
 }
--- echo/server/server.go
+++ echo/server/server.go
@@ -2,8 +2,11 @@ package server
 
 import (
 	"context"
+	"crypto/sha256"
+	"log"
 
 	"github.com/troydai/blocks/echo/proto"
+	"google.golang.org/grpc/metadata"
 )
 
 type (
@@ -17,8 +20,21 @@ func New() (proto.EchoServerServer, error) {
 }
 
 func (s *impl) Echo(ctx context.Context, req *proto.EchoRequest) (*proto.EchoResponse, error) {
+	log.Printf("recieve request\n")
+	if m, ok := metadata.FromIncomingContext(ctx); ok {
+		log.Printf("\tmetadata: len = %d", m.Len())
+		for k, v := range m {
+			log.Printf("\t\t %v: %v", k, v)
+		}
+	} else {
+		log.Println("\tmetadata: nil")
+	}
+
+	h := sha256.New()
+	h.Write([]byte(req.Message))
+
 	return &proto.EchoResponse{
 		Message: req.Message,
-		Digest:  "simple echo server",
+		Digest:  h.Sum(nil),
 	}, nil
 }
